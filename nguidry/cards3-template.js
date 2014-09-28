3a13cb80b6f14d8b9a2cd4eecd199b4c6c57a4dd
nguidry <nguidry7@gmail.com>
2014-09-28 16:01:02 -0700

    exercise 1 complete

--- nguidry/cards3-template.js
+++ nguidry/cards3-template.js
@@ -1,61 +1,59 @@
 function makeCard(id) {
-    //if (id < 0 || id > 51 || typeof id !== "number" || id % 1 !== 0) {
-      //  return NaN;
-    //}
     var card = {cardId: id};
     card.rank = makeCard.rank;
     card.suit = makeCard.suit;
-    card.color = makeCard.color;
+    card.cardColor = makeCard.cardColor;
     card.cardName = makeCard.cardName;
-    return card;
+    if (makeCard.isCard(card) === true) {
+      return card;
+    } else
+    return null;
 }
-//need to test
 
 //-----------------------
 // Methods to be called through factory:
 //-----------------------
 
-//makeCard.isCard = function(card) { // --> true,false
-
-//}
+makeCard.isCard = function(card) { // --> true,false
+    if ((typeof card)!="object") {
+            return null;
+    }
+    if (!("cardId" in card) || !card.rank || !card.suit || !card.cardColor || !card.cardName) {
+        return null;
+    }
+    if ((typeof card.cardId)!="number" || card.cardId%1 !== 0 || 
+            card.cardId < 0 || card.cardId > 51) {
+        return null;
+    }
+    return true;
+}
 
 //-----------------------------
 // Methods called though instances (where 'this' means the instance):
 //-----------------------------
 
-makeCard.isValid = function(num) { 
-    if ((typeof num)!="number")
-        return NaN;
-    if (num%1 !== 0) 
-        return NaN;
-    if (num<0 || num>51) 
-        return NaN;
-    return true;
-}
-
 makeCard.rank = function() { // --> 1..13, NaN
-    return makeCard.isValid(this.id) && Math.floor(this.id/4)+1;
+    return makeCard.isCard(this) &&
+            Math.floor(this.cardId/4)+1;
 };
 
 makeCard.suit = function() { // --> 1..4, NaN
-    return makeCard.isValid(this.id) &&
-        ((this.id%4)+1);
+    return makeCard.isCard(this) &&
+            (this.cardId%4)+1;
 };
    
-makeCard.color = function() { // -->"red,"black",NaN
-    var theSuit = this.suit(card); 
-    return theSuit && ((theSuit<3)? "red": "black");
+makeCard.cardColor = function() { // -->"red,"black",NaN
+    var suit=this.suit();
+    return suit && ((suit<3)? "red": "black");
 };
 
 makeCard.cardName = function() { //--> string, NaN
-    // This method can't have the key 'name' within the makeCard function,
-    // but instance objects can store a reference to it called 'name'
-        var ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
-        var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-        var cardRank = this.rank(card);
-        var cardSuit = this.suit(card);
-        return cardRank && cardSuit && (ranks[cardRank]+' of '+suits[cardSuit]);
-}
+    var ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+    var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+    var cardRank = this.rank() - 1;
+    var cardSuit = this.suit() - 1;
+    return cardRank && cardSuit && (ranks[cardRank] + " of " + suits[cardSuit]);
+};
 
 // Testing suite...
 function assert(claim,message) {
@@ -64,9 +62,6 @@ function assert(claim,message) {
 
 // card instances needed for assertions:
 var card0 = makeCard(0);
-//Object.getOwnPropertyNames(card0).forEach(function(val, idx, array) {
-//  console.log(våal + ' -> ' + card0[val]);
-//});
 var card3 = makeCard(3);
 var card5 = makeCard(5);
 var card51 = makeCard(51);
@@ -79,16 +74,16 @@ assert(card51.rank()===13,"Test 3 failed");
 assert(card0.suit()===1,  "Test 4 failed");
 assert(card5.suit()===2,  "Test 5 failed");
 assert(card51.suit()===4, "Test 6 failed");
-assert(card0.color()==='red',   "Test 10 failed");
-assert(card3.color()==='black', "Test 11 failed"); 
+assert(card0.cardColor()==='red',   "Test 10 failed");
+assert(card3.cardColor()==='black', "Test 11 failed");
 assert(card5.cardName()==='Two of Diamonds', "Test 12 failed");
 assert(card51.cardName()==='King of Clubs',  "Test 13 failed");
 
 // Test makeCard.isCard:
-//assert(makeCard.isCard(card0),  "Test 21 failed");
-//assert(makeCard.isCard(card51), "Test 22 failed");
-//assert(!makeCard.isCard(0)),    "Test 23 failed");
-//assert(!makeCard.isCard({})),   "Test 24 failed");
+assert(makeCard.isCard(card0),  "Test 21 failed");
+assert(makeCard.isCard(card51), "Test 22 failed");
+assert(!makeCard.isCard(0),    "Test 23 failed");
+assert(!makeCard.isCard({}),   "Test 24 failed");
 
 
 // Test card-making results:
@@ -100,9 +95,8 @@ assert(!makeCard(true),"Test 31 failed");
 
 
 // Test that methods are shared:
-assert(card0 !== card3, "Test 50 failed"); //first prove different cards
-assert(card0.rank === card3.rank, "Test 51 failed"); 
-assert(card0.suit === card3.suit, "Test 52 failed"); 
-assert(card0.card === card3.cardName, "Test 53 failed");
+assert(card0 !== card3, "Test 50 failed"); 
+assert(card0.rank === card3.rank, "Test 51 failed");
+assert(card0.suit === card3.suit, "Test 52 failed");
+assert(card0.cardName === card3.cardName, "Test 53 failed");
 //etc...
-
--- nguidry/deque-template.js
+++ nguidry/deque-template.js
@@ -1,40 +1,50 @@
 //-------
 // Part a): build a deque factory
 function makeDeque(values) {
-	// ...
+	var newDeque = values.slice(); 
+	
 };
 
 makeDeque.top = function() {
-	//...
+	//return the element on top (end of array) of the deque
 };
 makeDeque.bottom = function() {
-	//...
+	//return the element on bottom (beginning of array) of the deque
 };
 
 makeDeque.pop = function() {
-	//...
+	//remove and return the top (end of array) element
 };
 makeDeque.push = function(val) {
-	//...
+	//add an element to the top (end of array)
 };
 
 makeDeque.shift = function() {
-	//...
+	//remove and return the bottom (beginning of array) element
 };
 makeDeque.unshift = function(val) {
-	//...
+	//add an element to the bottom (beginning of array)
 };
 
-makeDeque.cut = function(offset) {
-	//...
+makeDeque.cut = function() {
+	//split the deque at the middle, then swap the two halves. 
+	//If there are an odd number of deque elements, split just above 
+	//the middle element. The element just below the split will become 
+	//the new top element when the halves are swapped.
 };
 
 makeDeque.map = function(convertValFn) {
-	//...
+	//return an array whose elements are derived from the values in 
+	//deque, converting each value with the function convertValFn(val)
 };
 
 makeDeque.sort = function(compareValsFn) {
-	//...
+	//reorder all elements of the deque according to the comparison 
+	//defined by the function compareValsFn, passed as an argument to 
+	//sort. compareValsFn(a,b) should return a positive number whenever 
+	//value a belongs somewhere below value b in the sorted result, and 
+	//a negative number whenever a belongs above b. (Zero means they're 
+	//equivalent: either may come first.)
 };
 
 var someCards = /* make array of 52 card objects here, using your code from Problem 1) */;
