790a4b250cd8c9f605232e83783e7d71c6093752
nguidry <nguidry7@gmail.com>
2014-09-28 08:19:11 -0700

    test commit

--- nguidry/cards3-template.js
+++ nguidry/cards3-template.js
@@ -2,7 +2,7 @@ function makeCard(id) {
     //if (id < 0 || id > 51 || typeof id !== "number" || id % 1 !== 0) {
       //  return NaN;
     //}
-    var card = {};
+    var card = {cardId: id};
     card.rank = makeCard.rank;
     card.suit = makeCard.suit;
     card.color = makeCard.color;
@@ -23,35 +23,39 @@ function makeCard(id) {
 // Methods called though instances (where 'this' means the instance):
 //-----------------------------
 
-makeCard.rank = function(card) { // --> 1..13, NaN
-    return Math.floor((this/4) + 1);
+makeCard.isValid = function(num) { 
+    if ((typeof num)!="number")
+        return NaN;
+    if (num%1 !== 0) 
+        return NaN;
+    if (num<0 || num>51) 
+        return NaN;
+    return true;
+}
+
+makeCard.rank = function() { // --> 1..13, NaN
+    return makeCard.isValid(this.id) && Math.floor(this.id/4)+1;
 };
 
-makeCard.suit = function(card) { // --> 1..4, NaN
-    return (this % 4) + 1;
+makeCard.suit = function() { // --> 1..4, NaN
+    return makeCard.isValid(this.id) &&
+        ((this.id%4)+1);
 };
    
-makeCard.color = function(card) { // -->"red,"black",NaN
-    if (this.suit() < 2) {
-            return "red";
-        } else {
-            return "black";
-          }
+makeCard.color = function() { // -->"red,"black",NaN
+    var theSuit = this.suit(card); 
+    return theSuit && ((theSuit<3)? "red": "black");
 };
 
-makeCard.cardName = function(card) { //--> string, NaN
+makeCard.cardName = function() { //--> string, NaN
     // This method can't have the key 'name' within the makeCard function,
     // but instance objects can store a reference to it called 'name'
-        var faces = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+        var ranks = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
         var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-        var cardFace = card.rank() - 1;
-        var cardSuit = card.suit() - 1;
-
-        return faces[cardFace] + " of " + suits[cardSuit];
-    }
-
-
-
+        var cardRank = this.rank(card);
+        var cardSuit = this.suit(card);
+        return cardRank && cardSuit && (ranks[cardRank]+' of '+suits[cardSuit]);
+}
 
 // Testing suite...
 function assert(claim,message) {
@@ -60,11 +64,9 @@ function assert(claim,message) {
 
 // card instances needed for assertions:
 var card0 = makeCard(0);
-Object.getOwnPropertyNames(card0).forEach(function(val, idx, array) {
-  console.log(val + ' -> ' + card0[val]);
-});
-
-
+//Object.getOwnPropertyNames(card0).forEach(function(val, idx, array) {
+//  console.log(våal + ' -> ' + card0[val]);
+//});
 var card3 = makeCard(3);
 var card5 = makeCard(5);
 var card51 = makeCard(51);
@@ -78,7 +80,7 @@ assert(card0.suit()===1,  "Test 4 failed");
 assert(card5.suit()===2,  "Test 5 failed");
 assert(card51.suit()===4, "Test 6 failed");
 assert(card0.color()==='red',   "Test 10 failed");
-assert(card3.color()==='black', "Test 11 failed"); //this test passed
+assert(card3.color()==='black', "Test 11 failed"); 
 assert(card5.cardName()==='Two of Diamonds', "Test 12 failed");
 assert(card51.cardName()==='King of Clubs',  "Test 13 failed");
 
@@ -99,8 +101,8 @@ assert(!makeCard(true),"Test 31 failed");
 
 // Test that methods are shared:
 assert(card0 !== card3, "Test 50 failed"); //first prove different cards
-assert(card0.rank === card3.rank, "Test 51 failed"); //this test passed
-assert(card0.suit === card3.suit, "Test 52 failed"); //this test passed
+assert(card0.rank === card3.rank, "Test 51 failed"); 
+assert(card0.suit === card3.suit, "Test 52 failed"); 
 assert(card0.card === card3.cardName, "Test 53 failed");
 //etc...
 
